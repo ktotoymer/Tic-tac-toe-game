83255cab0f5b86e8319aee451eb0660a32a55c5b
Mihail <mishaaalexeev@yandex.ru>
2024-03-28 23:39:21 +0300

    Отредактировал прототипы функция в заголовочном файле

--- tictactoefunc.h
+++ tictactoefunc.h
@@ -5,14 +5,12 @@
 #ifndef TIC_TAC_TOE_GAME_TICTACTOEFUNC_H
 #define TIC_TAC_TOE_GAME_TICTACTOEFUNC_H
 
-#define SIZE 3
+void show_pole(char **);
 
-void show_pole(char pole[][SIZE]);
+int is_win(char **);
 
-int is_win(char pole[][SIZE]);
+void winner(char **, int);
 
-void winner(char pole[][SIZE], int res);
-
-int check_position(char pole[][SIZE], int x, int y);
+int check_position(char **, int, int);
 
 #endif //TIC_TAC_TOE_GAME_TICTACTOEFUNC_H
